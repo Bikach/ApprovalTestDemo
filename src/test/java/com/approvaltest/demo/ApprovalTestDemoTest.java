0e8af2969e2696272492bd8af7e96d167932ac83
Bikach <chakibhoud.io@gmail.com>
2021-12-11 14:37:02 +0100

    init project

---
+++ src/test/java/com/approvaltest/demo/ApprovalTestDemoTest.java
@@ -0,0 +1,13 @@
+package com.approvaltest.demo;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ApprovalTestDemoTest {
+
+    @Test
+    public void doSomething(){
+
+        //Approvals.verify();
+    }
+}
