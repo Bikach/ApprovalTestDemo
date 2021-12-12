476d970b24507988a164fac2ba90c4e47d38f25f
Bikach <chakibhoud.io@gmail.com>
2021-12-12 16:00:43 +0100

    ready to demo

--- src/test/java/com/approvaltest/demo/ApprovalTestDemoTest.java
+++ src/test/java/com/approvaltest/demo/ApprovalTestDemoTest.java
@@ -1,14 +1,11 @@
 package com.approvaltest.demo;
 
-import org.approvaltests.Approvals;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class ApprovalTestDemoTest {
 
     @Test
     public void doSomething(){
-
-        //Approvals.verify();
+        // do something
     }
 }
